b9b1f4e5efa863afc646349a78aa22a1fd01f5d0
Navjot Singh Minhas <navjotsingh548@gmail.com>
2020-10-23 03:03:35 +0530

    Add files via upload

---
+++ precache-manifest.9295a25fb5218eb76e7b6267845e4863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1284944c8b8b9cf6d0fa546c3e62123c",
+    "url": "/ReactTest/index.html"
+  },
+  {
+    "revision": "394c66db418156477de0",
+    "url": "/ReactTest/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fb12b7ec723f3ff83dba",
+    "url": "/ReactTest/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "394c66db418156477de0",
+    "url": "/ReactTest/static/js/2.d526fae4.chunk.js"
+  },
+  {
+    "revision": "fb12b7ec723f3ff83dba",
+    "url": "/ReactTest/static/js/main.6371cd8e.chunk.js"
+  },
+  {
+    "revision": "f50b17a0f66a98e8df07",
+    "url": "/ReactTest/static/js/runtime~main.cec9f60b.js"
+  }
+]);
