96bdf3e5ad37b7a2fa13f0c377eeb43bf22d59d5
Navjot Singh Minhas <navjotsingh548@gmail.com>
2020-10-23 02:54:09 +0530

    Add files via upload

---
+++ precache-manifest.2f5b2b59b2b218058e24d50f25abc9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a5843998cffd7752d8f91f00ad30af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394c66db418156477de0",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fb12b7ec723f3ff83dba",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "394c66db418156477de0",
+    "url": "/static/js/2.d526fae4.chunk.js"
+  },
+  {
+    "revision": "fb12b7ec723f3ff83dba",
+    "url": "/static/js/main.6371cd8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
